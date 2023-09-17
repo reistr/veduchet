386a02d6242045497e78ca6b68622e6991d3ef6e
Reistr <egoran@inbox.ru>
2023-09-17 23:41:18 +0300

    feat: style 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,8 @@
 import * as React from "react";
 import { Link, HeadFC, PageProps } from "gatsby";
 import { PageHead } from "../components/Seo";
+import { Page } from "../components/Page";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
@@ -16,32 +12,19 @@ const headingStyles = {
 const paragraphStyles = {
   marginBottom: 48,
 };
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Страница не найдена</h1>
-      <p style={paragraphStyles}>
-        Извините, запрошенная страница не существует.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
+    <Page>
+      <section className="section">
+        <h2 style={headingStyles}>Страница не найдена</h2>
+        <p style={paragraphStyles}>
+          Извините, запрошенная страница не существует.
+          <br />
+        </p>
         <Link to="/">Перейти на главную страницу</Link>.
-      </p>
-    </main>
+      </section>
+    </Page>
   );
 };
 
