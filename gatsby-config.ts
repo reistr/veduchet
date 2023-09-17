b44c1afbafe1263652178874ca7009e5ec80196c
Reistr <egoran@inbox.ru>
2023-09-17 21:27:56 +0300

    feat: improve PWA support

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,13 +2,13 @@ import type { GatsbyConfig } from "gatsby";
 import dotenv from "dotenv";
 
 dotenv.config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: ".env.${process.env.NODE_ENV}",
 });
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `distantuchet`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "distantuchet",
+    siteUrl: "https://distantuchet.netlify.app/",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -20,9 +20,16 @@ const config: GatsbyConfig = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
+        name: "Distantuchet contact form",
+        short_name: "Distantuchet",
+        start_url: "/",
+        background_color: "#86BDBF",
+        theme_color: "#3E9599",
+        display: "standalone",
         icon: "src/images/logo.svg",
       },
     },
+    "gatsby-plugin-offline",
   ],
 };
 
